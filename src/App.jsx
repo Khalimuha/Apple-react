e53561f085149ca417cf79531ef7c2d8ade88d48
Khalimuha <kalimuha2016@gmail.com>
2025-04-13 22:17:57 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import AlertSection from './components/alertSection/AlertSection';
+import SecetionOne from './components/sectionOne/SectionOne';
+import SectionTwo from './components/sectionTwo/SectionTwo';
+import SectionThree from './components/sectionThree/SectionThree';
+import SectionFour from './components/sectionFour/SectionFour';
+import SectionFive from './components/SectionFive/SectionFive';
+import SectionSix from './components/sectionSix/SecetionSix';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Header />
+
+        <AlertSection />
+
+        <SecetionOne />
+
+        <SectionTwo />
+
+        <SectionThree />
+
+        <SectionFour />
+
+        <SectionFive />
+
+        <SectionSix />
+
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App
